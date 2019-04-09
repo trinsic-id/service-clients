80ed3ac843a7a8ee7f616e57009cfa32a8c304b7
Tomislav Markovski <tmarkovski@gmail.com>
2019-04-09 17:10:46 -0400

    Add solution

--- test/ServiceClient.Tests/UnitTest1.cs
+++ test/ServiceClient.Tests/UnitTest1.cs
@@ -3,27 +3,24 @@ using System.Threading.Tasks;
 using Microsoft.Rest;
 using Streetcred.Cloud.Client;
 using Streetcred.Cloud.Client.Models;
+using Streetcred.Portal.Client;
 using Xunit;
 
 namespace ServiceClient.Tests
 {
     public class UnitTest1
     {
-        private string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1NDgwMzgyMDIsIm5iZiI6MTU0ODAzNDYwMiwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9zdHJlZXRjcmVkaWQuYjJjbG9naW4uY29tLzkyOTgxN2U5LWQ5YjgtNGFiZi1iNDJmLWNkMDQ1MDYwYWI5Ny92Mi4wLyIsInN1YiI6IjIwZTBhMzU2LTk0NTQtNDgyZC1hM2Y0LTU3ZGNlYzgyMzUxYiIsImF1ZCI6IjNhNjFiOGUzLTkyNzgtNDk1Zi04ODliLTJlMmZjYjUxZDBjMyIsIm5vbmNlIjoiYmNiOGRhZGQtMzcyZi00YmJhLWI1OTktYmZkNzE3ZGE0OWFjIiwiaWF0IjoxNTQ4MDM0NjAyLCJhdXRoX3RpbWUiOjE1NDgwMzQ2MDIsImdpdmVuX25hbWUiOiJUb21pc2xhdiIsImZhbWlseV9uYW1lIjoiTWFya292c2tpIiwibmFtZSI6IlRvbWlzbGF2IE1hcmtvdnNraSIsImlkcCI6ImZhY2Vib29rLmNvbSIsIm9pZCI6IjIwZTBhMzU2LTk0NTQtNDgyZC1hM2Y0LTU3ZGNlYzgyMzUxYiIsImVtYWlscyI6WyJ0bWFya292c2tpQGdtYWlsLmNvbSJdLCJ0ZnAiOiJCMkNfMV9zdXNpIn0.KsShJg2N_PgIvsA2yhTyWMlDvTjKoyV2qPx5U21EZZrC89HAfb4DyfTdpgqjDIfNXBdUK_X5Zm96Bhe07HEIBk3w0V95nn3Mzio5aFHyeFuAlRCBDCnSRRZeYgePEkSqsODU7jGFgvNBs9Wjay0MU65BAHDLqnik7djN9hNiSIBOiyPPocUObKtuSKcAKqV4xXQWMeZ3I8YHV3eM1s660yUun7fhWblo95o9DuYRgmlxq-jyzponxZp-SqFl3_D857IlV7MfIasoriR1Kjk_5IK0EnxVgWd0psVemwiv0vKYndQKheccM-9xXbEsMi4mHhXU0DBy_Nt7Pw22SPqpRA";
+        private string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1NTQ5MjczNTMsIm5iZiI6MTU1NDg0MDk1MywidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9zdHJlZXRjcmVkaWQuYjJjbG9naW4uY29tLzkyOTgxN2U5LWQ5YjgtNGFiZi1iNDJmLWNkMDQ1MDYwYWI5Ny92Mi4wLyIsInN1YiI6IjljMzZkMmRiLTlhMGMtNDhhYS1hMWJlLTlhMmY0ZjM5Yzc0NiIsImF1ZCI6IjNhNjFiOGUzLTkyNzgtNDk1Zi04ODliLTJlMmZjYjUxZDBjMyIsIm5vbmNlIjoiMWU0YzczNjYtMGJkNy00ZmQ0LWExNDctOTdjZjI1ZWYyOWI1IiwiaWF0IjoxNTU0ODQwOTUzLCJhdXRoX3RpbWUiOjE1NTQ4NDA5NTMsIm9pZCI6IjljMzZkMmRiLTlhMGMtNDhhYS1hMWJlLTlhMmY0ZjM5Yzc0NiIsImZhbWlseV9uYW1lIjoiTWFya292c2tpIiwiZ2l2ZW5fbmFtZSI6IlRvbWlzbGF2IiwibmFtZSI6InVua25vd24iLCJlbWFpbHMiOlsidG1hcmtvdnNraUBnbWFpbC5jb20iXSwidGZwIjoiQjJDXzFfc3VzaSJ9.a-EChSWck88CQ3Au_qEZNYd87AvS4cWwYL9u8IUJqb4lniKVhrmfkW1MKxoQe9GqvBvtsI5ysLmqQOqLDVNo_pAZw01nedS5vcF5FkgJewXLpN0jS_gCU5aURTgue97n4pIomYlVQ7Kz2HSpqy3Y008o15H2jD6GvRPrRb_MfV8ZVBqmnfFlp8qPl0dZBZaE3TJvCuDcBKZBWk-zZ7nL4_sEfPQnMoeC1GGr2iMSvrmG6Epux-GDMKJOB8seGnGP6H0JWTXCLYFvsq5BQdlt14dJ-4NrEGjp4Vwz-FcktTy-XJlQNYVfVo0nrpOmq7tgp7GhSkHTQ-TDwFDWE1K2zQ";
 
         [Fact]
         public async Task Test1()
         {
-            var client = new CloudServiceClient(new TokenCredentials(accessToken))
+            var client = new PortalServiceClient(new TokenCredentials(accessToken))
             {
-                BaseUri = new Uri("http://localhost:5009/")
+                BaseUri = new Uri("http://localhost:5000/")
             };
 
-            var walletId = await client.CreateWalletAsync(new WalletInfo
-            {
-                Name = "My Wallet",
-                Type = "edge"
-            });
+            var walletId = await client.ListProofRequestsAsync("fXX2pUFOvnSlEzwRyde8K83d");
         }
     }
 }
