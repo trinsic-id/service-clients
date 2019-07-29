049f0e6eb894e4aa35260745ba06a06f986f1192
Tomislav Markovski <tmarkovski@gmail.com>
2019-07-29 08:44:09 -0400

    Add client configuration for azure API

---
+++ csharp/ServiceClientOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    public class ServiceClientOptions
+    {
+        public string AccessToken { get; set; }
+        public string SubscriptionKey { get; set; }
+    }
+}
---
+++ csharp/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Rest;
+using Streetcred.ServiceClients;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStreetcredClient(this IServiceCollection services, Action<ServiceClientOptions> configure)
+        {
+            services.Configure(configure);
+            services.AddSingleton<StreetcredClientCredentials>();
+            services.AddSingleton<IAgencyServiceClient, AgencyServiceClient>(provider =>
+                new AgencyServiceClient(provider.GetRequiredService<StreetcredClientCredentials>()));
+            return services;
+        }
+
+        public static IServiceCollection AddStreetcredClient(this IServiceCollection services)
+        {
+            return AddStreetcredClient(services, options => { });
+        }
+    }
+}
