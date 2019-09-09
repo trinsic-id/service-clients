2a4538dbf344047b80a07dd455bf13c985589828
Tomislav Markovski <tmarkovski@gmail.com>
2019-09-09 15:28:17 -0400

    Update azure-pipelines.yml for Azure Pipelines (#4)
    Add Typescript package build

--- typescript/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import { AgencyServiceClient, AgencyServiceModels } from "./agencyServiceClient"
-import { StreetcredClientCredentials } from "./streetcredClientCredentials"
-
-export {
-    AgencyServiceClient,
-    AgencyServiceModels as Models,
-    StreetcredClientCredentials as Credentials
-};
