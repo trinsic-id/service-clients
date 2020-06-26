0c95dd0ac6ac08bb5035af1947ad06ccd984a11c
Michael Boyd <michaeldavisboyd@gmail.com>
2020-06-25 23:03:55 -0700

    changed build script

--- csharp/build.sh
+++ csharp/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-autorest --input-file=http://streetcred-cw-api-staging.azurewebsites.net/swagger/v1/swagger.json --csharp --output-folder=Generated --namespace=Streetcred.ServiceClients --add-credentials --override-client-name=CustodianServiceClient
-autorest --input-file=http://streetcred-agency-api-staging.azurewebsites.net/swagger/v1/swagger.json --csharp --output-folder=Generated --namespace=Streetcred.ServiceClients --add-credentials --override-client-name=AgencyServiceClient
+autorest --input-file=../openapi/custodian.json --csharp --output-folder=Generated --namespace=Streetcred.ServiceClients --add-credentials --override-client-name=CustodianServiceClient
+autorest --input-file=../openapi/agency.json --csharp --output-folder=Generated --namespace=Streetcred.ServiceClients --add-credentials --override-client-name=AgencyServiceClient
 autorest --input-file=../openapi/management.json --csharp --output-folder=Generated --namespace=Streetcred.ServiceClients --add-credentials --override-client-name=ManagementServiceClient
 
 dotnet pack
